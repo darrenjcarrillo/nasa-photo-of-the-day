fd8a048af0b6a19e9e1bcbdc00c4f6cc0901b79b
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-09-11 15:40:05 -0700

    Tried making separate components. Ended up writing everything on the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+// import NasaApod from "./components/NasaApod";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  const [image, setImage] = useState();
+  const [date, setDate] = useState();
+  const [title, setTitle] = useState();
+
+  // Use Effect
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        console.log(response);
+        const spaceImage = response.data.hdurl;
+        // console.log("Nasa Image", spaceImage);
+        setImage(spaceImage);
+
+        const date = response.data.date;
+        // console.log("The date is:", date);
+        setDate(date);
+
+        const title = response.data.title;
+        console.log("the Title:", title);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
